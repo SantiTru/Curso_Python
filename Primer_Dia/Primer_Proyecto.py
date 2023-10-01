24206d5e9db10099e41ec05e4a3edb4b31e81700
SantiTru <santiagotrujillonavarro@gmail.com>
2023-10-02 00:57:35 +0200

    Commit

--- Primer_Dia/Primer_Proyecto.py
+++ Primer_Dia/Primer_Proyecto.py
@@ -1,3 +1,3 @@
-print('Vamos aponerle nombre a tu cerveza\n')
+print('\nVamos a ponerle nombre a tu cerveza\n')
 print('Pues ya tienes el nombre de tu cerveza: \n' + '"' + (input('¿Cual es tu comida favorita?: ')) + ' ' +
       (input('¿Como se dice cerveza en inglés?: ')) + '".')
