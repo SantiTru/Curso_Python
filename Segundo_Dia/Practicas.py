48c91a3806030b17874f97e97879e5bd79aba6ff
SantiTru <santiagotrujillonavarro@gmail.com>
2023-10-02 18:35:51 +0200

    Commit

---
+++ Segundo_Dia/Practicas.py
@@ -0,0 +1,63 @@
+"""
+nombre = "Juan"
+print(nombre)
+
+nombre = "Laura"
+print(nombre)
+
+edad = 30
+print(edad)
+edad2 =15
+print(edad+edad2)
+
+nombre= input("Dime tu nombre: ")
+
+print("Tu nombre es: " + nombre)
+
+edadTotal= edad + edad2
+print(edadTotal)
+
+mi_numero = 1 + 3
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+
+mi_numero = 5.8
+mi_numero = mi_numero + mi_numero
+print(mi_numero)
+print(type(mi_numero))
+
+num1 = 7.5
+num2 = 2.5
+print(type(num1 + num2))
+
+num1 = 20
+num2 = 30.5
+
+num1 = num1 +num2
+
+print(type(num1))
+print(type(num2))
+
+num1 = 5.8
+print(num1)
+print(type(num1))
+
+num2 = int(num1)
+print(num2)
+print(type(num2))
+
+edad = input("Dime tu edad: ")
+edad = int(edad)
+nueva_edad = 1 + edad
+print(nueva_edad)
+"""
+
+x = 10
+y = 5
+
+print("Mis números son " + str(x) + " y " + str(y))
+
+color = "rojo"
+matricula = 541926
+
+print(f"El auto es {color} y su matricuka es {matricula}")
